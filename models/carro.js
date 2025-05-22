ac0d783e6f48d86df669c92988da5a2999a51137
MatheusSalera <mspittar@gmail.com>
2025-05-22 15:56:09 -0300

    js

--- models/carro.js
+++ models/carro.js
@@ -9,25 +9,23 @@ const carroSchema = new mongoose.Schema({
 
 module.exports = mongoose.model('Carro', carroSchema);
 
-// Cadastrar novo carro
+
 app.post('/carros/novo', async (req, res) => {
   await Carro.create(req.body);
   res.redirect('/carros/gerenciar');
 });
 
-// Atualizar carro
 app.post('/carros/editar/:id', async (req, res) => {
   await Carro.findByIdAndUpdate(req.params.id, req.body);
   res.redirect('/carros/gerenciar');
 });
 
-// Remover carro
+
 app.post('/carros/remover/:id', async (req, res) => {
   await Carro.findByIdAndDelete(req.params.id);
   res.redirect('/carros/gerenciar');
 });
 
-// Vender carro
 app.post('/carros/vender/:id', async (req, res) => {
   const carro = await Carro.findById(req.params.id);
   if (carro.qtde_disponivel > 0) {
