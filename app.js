073a5a3f81494025a08f00d850483ca5a13554f1
MatheusSalera <mspittar@gmail.com>
2025-05-22 15:54:40 -0300

    att js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-// app.js
+
 const mongoose = require('mongoose');
 mongoose.connect('mongodb+srv://mspittar:<db_password>@projetos.qrfo6xo.mongodb.net/?retryWrites=true&w=majority&appName=Projetos', { useNewUrlParser: true, useUnifiedTopology: true });
 
-// app.js
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('
